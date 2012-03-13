fa3903a2daf10b55616d97e0bc8b3d58f2928c98
Gabriel KLEIN <contact@gabriel-klein.com>
2012-03-13 15:31:05 -0700

    first commit

--- chef_setup.sh
+++ chef_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | sudo tee /etc/apt/sources.list.d/opscode.list
-sudo mkdir -p /etc/apt/trusted.gpg.d
+echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | tee /etc/apt/sources.list.d/opscode.list
+mkdir -p /etc/apt/trusted.gpg.d
 gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
 gpg --export packages@opscode.com | sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null
-sudo apt-get update
-sudo apt-get install opscode-keyring -y
+apt-get update
+apt-get install opscode-keyring -y
+apt-get upgrade
 #sudo apt-get install chef chef-server -y
