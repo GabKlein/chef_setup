91bebb2a35a221158ae7e6a01c466481fdf35a10
Gabriel KLEIN <contact@gabriel-klein.com>
2012-03-13 21:50:02 -0700

    first commit

--- chef_setup.sh
+++ chef_setup.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | tee /etc/apt/sources.list.d/opscode.list
-mkdir -p /etc/apt/trusted.gpg.d
-gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
-gpg --export packages@opscode.com | sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null
-apt-get update
-apt-get install opscode-keyring -y
+
+#echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | tee /etc/apt/sources.list.d/opscode.list
+#mkdir -p /etc/apt/trusted.gpg.d
+#gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
+#gpg --export packages@opscode.com | sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null
+#apt-get update
+#apt-get install opscode-keyring -y
 #apt-get upgrade -y
 #sudo apt-get install chef chef-server -y
+#!/bin/bash -x
+
+rabbitmqctl add_vhost /chef
+rabbitmqctl add_user chef changeme
+rabbitmqctl set_permissions -p /chef chef ".*" ".*" ".*"
+cd /home/ubuntu
+su - ubuntu -c "mkdir -p ~/.chef"
+su - ubuntu -c "sudo cp /etc/chef/validation.pem /etc/chef/webui.pem ~/.chef"
+su - ubuntu -c "sudo chown -R ubuntu ~/.chef"
+su - ubuntu -c "knife configure -i" <<EOF
+
+http://localhost:4000
+
+
+/home/ubuntu/.chef/webui.pem
+
+/home/ubuntu/.chef/validation.pem
+
+EOF
